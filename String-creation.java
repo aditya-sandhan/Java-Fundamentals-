fe70786fd2b4d254632b12d9687ad0e3b00ff4e7
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-26 16:55:09 +0530

    Testing String creation techniques

---
+++ String-creation.java
@@ -0,0 +1,13 @@
+public class teststring{
+    pubic static void main(String[] args){
+
+        String S1 = "Adi";
+        String S2 ="Adi";
+
+        String S3 = new String("Adi");
+        String S4 = new String("Adi");
+
+        System.out.println(S1==S2); // true
+        System.out.println(S3==S4); // false
+    }
+}
