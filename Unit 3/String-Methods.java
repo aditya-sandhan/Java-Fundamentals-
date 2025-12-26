3d22b0fe36b7fa16416532f3ab26041aa7833667
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-27 00:09:00 +0530

    String Operations

---
+++ Unit 3/String-Methods.java
@@ -0,0 +1,12 @@
+public class teststring{
+    public static void main(String[] args){
+
+        String S1 = "Aditya sandhan is a Entrepreneur";
+        String S2 ="Aditya sandhan is a Entrepreneur";
+
+        System.out.println(S1.length()); 
+        System.out.println(S1.charAt(4));
+        System.out.println(S1.equals(S2)); 
+        System.out.println(S1.substring(3,10));
+    }
+} 
--- Unit 3/String-creation.java
+++ Unit 3/String-creation.java
@@ -1,10 +1,10 @@
 public class teststring{
     pubic static void main(String[] args){
 
-        String S1 = "Adi";
+        String S1 = "Adi"; // String Literals (Stored inside SCP- Strng constant pool )
         String S2 ="Adi";
 
-        String S3 = new String("Adi");
+        String S3 = new String("Adi"); // Created using new keyword (Stored inside Heap memory)
         String S4 = new String("Adi");
 
         System.out.println(S1==S2); // true
