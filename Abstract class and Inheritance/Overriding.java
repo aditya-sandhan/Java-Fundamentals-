482f5bbd0ea9402a83855e441f4df681cd25f281
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-27 19:06:23 +0530

    Overriding methods

---
+++ Abstract class and Inheritance/Overriding.java
@@ -0,0 +1,18 @@
+class animal {
+    void sound{
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class dog extends animal {
+    @Override 
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+
+public class Overriding {
+    public static void main(String[] args){
+        animal myAnimal = new dog(); //Dynamic method dispatch
+        myAnimal.sound();
+}
