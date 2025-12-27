8447cd199b513eb1dfecc9fd891693a96fd0c5f0
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-27 13:38:45 +0530

    Playong with Arrays- 1D 2D

---
+++ Unit 3/1D-array.java
@@ -0,0 +1,18 @@
+public class arry{
+    public static void main(String[] args){
+        //declaration and memory allocation
+        int[] arr = new int[5];
+
+        //initialization
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+
+        //accessing elements
+        for(int i=0; i<arr.length; i++){
+            System.out.println("Element at index " + i + ": " + arr[i]);
+        }
+    }
+}
