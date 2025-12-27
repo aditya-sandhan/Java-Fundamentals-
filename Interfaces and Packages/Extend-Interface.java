ae3462d979d1e216ff4cb4f3c3362ccd8d49733a
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-27 21:46:24 +0530

    Extending Interfaces To achieve Multiple inheritance

---
+++ Interfaces and Packages/Extend-Interface.java
@@ -0,0 +1,31 @@
+interface A{
+    void methodA();
+}
+
+interface B{
+    void methodB();
+}
+
+interface C extends A,B{           //Multiple inheritance using interfaces
+    void methodC();
+}
+
+class demo implements C{
+    public void methodA(){
+        System.out.println("Implementation of methodA from interface A");
+    }
+    public void methodB(){
+        System.out.println("Implementation of methodB from interface B");
+    }
+    public void methodC(){
+        System.out.println("Implementation of methodC from interface C");
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        demo obj = new demo();
+        obj.methodA();
+        obj.methodB();
+        obj.methodC();
+    }
+}
