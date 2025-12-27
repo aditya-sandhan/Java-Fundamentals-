4eb28d70eafb9389cd92d1759643fe32eb9b9c6a
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-27 16:41:48 +0530

    Command line argument operations

---
+++ Unit 3/CLA.java
@@ -0,0 +1,17 @@
+public class testCLA{
+    public static void main(String[] args){
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[1]);
+
+
+    }
+}
+
+// Terminal :
+// java testCLA Hello World Welcome
+// Output :
+// Hello
+// World
+// Welcome
