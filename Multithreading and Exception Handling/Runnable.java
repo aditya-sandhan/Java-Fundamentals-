0b900ad415d4dcd4878e234d03f4945d43f09b1e
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-28 14:43:06 +0530

    Add Runnable and Thread implementations for multithreading examples

---
+++ Multithreading and Exception Handling/Runnable.java
@@ -0,0 +1,12 @@
+class mytask implements Runnable{       // implementing Runnable interface
+    public void run(){
+        System.out.println("My task is running");
+    }
+}
+public class test{
+    public static void main(String[] args){
+        mytask obj = new mytask();      //creating object of mytask class
+        Thread t = new Thread(obj);     //creating thread object and passing mytask class object
+        t.start();    // creates new thread memory and calls run() automatically internally
+    }
+}
---
+++ Multithreading and Exception Handling/Thread.java
@@ -0,0 +1,12 @@
+class Mythread extends Thread{      //extending Thread class
+    public void run(){          // 
+        System.out.println("My thread is running");
+    }
+}
+
+public class Test {
+    public static void main(String[] args){
+        Mythread t = new Mythread(); //creating thread object
+        t.start();
+    }
+}
