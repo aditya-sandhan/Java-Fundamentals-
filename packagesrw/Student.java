ac12b175e3ed083d18c5c5e93585c42f205ba772
Aditya Sandhan <adityasandhan.ai@gmail.com>
2025-12-28 10:30:36 +0530

    package creation

---
+++ packagesrw/Student.java
@@ -0,0 +1,10 @@
+package packagesrw; // Package creation 
+
+public class Student {      //class inside packages 
+    public void show(){
+        System.out.println("This is a student class in interfaces.and.packages package.");
+    }
+}
+
+// we can access it in another file by importing it 
+//just like JAVA API packages
